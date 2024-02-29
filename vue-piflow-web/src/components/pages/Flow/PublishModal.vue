73110c3cd092e6aa256dc326377ddad79fa73ac5
zk <1350534586@qq.com>
2024-02-29 14:49:32 +0800

    发布修改

--- vue-piflow-web/src/components/pages/Flow/PublishModal.vue
+++ vue-piflow-web/src/components/pages/Flow/PublishModal.vue
@@ -1,11 +1,20 @@
 <template>
+  <Drawer
+    width="600"
+    :title="formData.id ? '编辑' : '发布'"
+    :scrollabl="true"
+    :closable="false"
+    v-model="open"
+  >
+    <!-- 
   <Modal
     v-model="open"
     :title="formData.id ? '编辑' : '发布'"
     :ok-text="$t('modal.ok_text')"
     :cancel-text="$t('modal.cancel_text')"
     width="600"
-  >
+    @on-ok="handleSaveUpdateData"
+  > -->
     <div class="modal-warp">
       <Form
         ref="formValidate"
@@ -25,7 +34,7 @@
             :placeholder="$t('modal.placeholder_select')"
             :normalizer="normalizer"
             :options="treeData"
-            style="width: 350px; height: 32px"
+            style="width: 100%; height: 32px"
           />
         </FormItem>
         <FormItem label="描述：" prop="description">
@@ -35,7 +44,76 @@
           ></Input>
         </FormItem>
         <h4>选择发布组件</h4>
-        <div v-for="(item, index) in stops" label="" class="stop" :key="index">
+
+        <Collapse simple>
+          <Panel :name="item.id" v-for="(item, index) in stops" :key="index">
+            <span
+              ><Checkbox v-model="item.checked"></Checkbox>
+              {{ item.stopName }}</span
+            >
+
+            <div slot="content">
+              <Collapse simple>
+                <Panel
+                  :name="child.id"
+                  v-for="(child, idx) in item.stopPublishingPropertyVos"
+                  :key="idx"
+                >
+                  <span
+                    ><Checkbox v-model="child.checked"></Checkbox>
+                    {{ child.propertyName }}</span
+                  >
+                  <div slot="content">
+                    <div class="item">
+                      <label>发布名称：</label>
+                      <Input
+                        v-model="child.name"
+                        show-word-limit
+                        maxlength="100"
+                        :placeholder="$t('modal.placeholder')"
+                        style="width: 200px"
+                      />
+                    </div>
+                    <div class="item">
+                      <label>发布类型：</label>
+                      <Cascader :data="cascaderData" trigger="hover" v-model="child.cascaderType"
+                      ></Cascader>
+                    </div>
+                    <div
+                      class="item"
+                      v-if="child.cascaderType[1] === 0"
+                      @click="handleClickUpload(index, idx)"
+                    >
+                      <label>上传文件：</label>
+                      <div>
+                        <Upload
+                          action="/aaa"
+                          :before-upload="handleBeforeUpload"
+                        >
+                          <Button icon="ios-cloud-upload-outline"
+                            >Upload files</Button
+                          >
+                        </Upload>
+                        <p>{{ child.fileName }}</p>
+                      </div>
+                    </div>
+                    <div class="item"  v-if="child.cascaderType[1] === 1">
+                      <label>自定义值：</label>
+                      <Input
+                        disabled
+                        v-model="child.customValue"
+                        :placeholder="$t('modal.placeholder')"
+                        style="width: 200px"
+                      />
+                    </div>
+                  </div>
+                </Panel>
+              </Collapse>
+            </div>
+          </Panel>
+        </Collapse>
+
+        <!-- <div v-for="(item, index) in stops" label="" class="stop" :key="index">
           <div class="stop_checked">
             <p>{{ item.stopName }}</p>
             <label><Checkbox v-model="item.checked"></Checkbox></label>
@@ -65,8 +143,6 @@
               <div class="item">
                 <label>发布类型：</label>
                 <Radio-group v-model="child.type">
-                  <!-- <Radio :label="0">文件</Radio>
-                  <Radio :label="1">普通</Radio> -->
                   <Radio :label="1">输入</Radio>
                   <Radio :label="2">输出</Radio>
                 </Radio-group>
@@ -105,10 +181,10 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
       </Form>
     </div>
-    <div slot="footer">
+    <div class="footer">
       <Button @click="open = false" class="custom-btn-default">{{
         $t("modal.cancel_text")
       }}</Button>
@@ -116,7 +192,8 @@
         $t("modal.confirm")
       }}</Button>
     </div>
-  </Modal>
+    <!-- </Modal> -->
+  </Drawer>
 </template>
 
 <script>
@@ -140,6 +217,26 @@ export default {
       stops: [],
       treeData: [],
       fileList: [],
+      cascaderData: [
+        {
+          label: "输入",
+          value: 1,
+          children: [
+            {
+              label: "文件输入",
+              value: 0,
+            },
+            {
+              label: "普通输入",
+              value: 1,
+            },
+          ],
+        },
+        {
+          label: "输出",
+          value: 2,
+        },
+      ],
       ruleValidate: {
         name: [
           {
@@ -176,6 +273,7 @@ export default {
             fileName: item.fileName,
             name: item.name,
             type: item.type,
+            cascaderType:[],
             allowableValues: item.allowableValues,
             customValue: item.customValue,
             description: item.description,
@@ -219,11 +317,12 @@ export default {
             );
             this.stops[index].stopPublishingPropertyVos[idx].checked = true;
             this.stops[index].stopPublishingPropertyVos[idx].name = child.name;
-            this.stops[index].stopPublishingPropertyVos[idx].fileName =
-              child.fileName;
+            this.stops[index].stopPublishingPropertyVos[idx].fileId = child.fileId;
+            this.stops[index].stopPublishingPropertyVos[idx].fileName = child.fileName;
             this.stops[index].stopPublishingPropertyVos[idx].publishingId =
               child.publishingId;
             this.stops[index].stopPublishingPropertyVos[idx].type = child.type;
+            this.stops[index].stopPublishingPropertyVos[idx].cascaderType = child.type === 2 ?[child.type]:[1,child.type];
           });
         });
       }
@@ -234,7 +333,7 @@ export default {
       this.$event.emit("loading", true);
       // 获取分类信息
       const productNode = findTree(this.treeData, this.formData.productTypeId);
-      this.formData.productTypeId = Number(this.formData.productTypeId)
+      this.formData.productTypeId = Number(this.formData.productTypeId);
       this.formData.productTypeName = productNode.name;
       this.formData.productTypeDescription = productNode.description;
 
@@ -247,14 +346,16 @@ export default {
           stopPublishingPropertyVos: item.stopPublishingPropertyVos.filter(
             (prop) => {
               if (prop.checked) {
-                if (prop.type === 1) {
-                  //输出类型
-                  if (prop.type === 0 && !prop.fileName) {
+                  //输入类型
+                if (prop.cascaderType.length === 2  ) {
+                  console.log(prop)
+                  if (prop.cascaderType[1] === 0 && !prop.fileName) {
                     //文件
                     errMsg = `请确保组件 ${item.stopName} 的参数 ${prop.propertyName} 的文件不为空`;
                   }
-                  prop.type = prop.type1;
-                  delete prop.type1
+                  prop.type = prop.cascaderType[1];
+                }else{
+                  prop.type = prop.cascaderType[0];
                 }
               }
               return prop.checked;
@@ -377,6 +478,15 @@ export default {
     text-align: left;
   }
 }
+
+.ivu-collapse {
+  border: 0;
+  > .ivu-collapse-item {
+    box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.06);
+    border: none;
+    margin: 0 10px 5px;
+  }
+}
 .stop {
   border: 1px solid #eee;
   margin-bottom: 5px;
@@ -412,6 +522,30 @@ h4 {
 ::v-deep .ivu-modal {
   max-height: 80vh;
   overflow: auto;
+}
+
+::v-deep .ivu-drawer-body {
+  height: calc(100% - 51px);
+  background: rgb(247, 248, 250);
+  padding: 0;
+  input {
+    border-radius: 6px;
+  }
+  .modal-warp {
+    padding: 16px;
+    height: calc(100% - 40px);
+    overflow: auto;
+  }
+  .footer {
+    height: 40px;
+    padding-left: 20px;
+    border-top: 1px solid #dedede;
+    display: flex;
+    align-items: center;
+    button {
+      margin-right: 10px;
+    }
+  }
 }
 </style>
 
